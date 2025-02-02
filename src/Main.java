9698ad70e027a0a4026449535727414fa18e7ff8
Dylan Vines <vinesdn@mail.uc.edu>
2025-02-01 21:40:47 -0500

    Completed Lab_04_03

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-
+        int creditCardBalance = 5000;
+        double interestRate = .17 / 12;
+        double monthOneInterest = creditCardBalance * interestRate;
+        double monthOneBalance = creditCardBalance + monthOneInterest;
+        double monthTwoInterest = monthOneBalance * interestRate;
+        double monthTwoBalance = monthOneBalance + monthTwoInterest;
+        System.out.println("Your interest rate after one month is " + monthOneInterest + " bringing your balance to " + monthOneBalance + ".");
+        System.out.println ("Your interest after two months with no payment is " + monthTwoInterest + " bringing your total balance to " + monthTwoBalance + ".");
     }
 }
